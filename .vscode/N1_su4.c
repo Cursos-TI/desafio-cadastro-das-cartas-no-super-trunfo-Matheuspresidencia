1e330ee3b4faf25a657f98e92b32bbe423225428
Matheuspresidencia <mateusuniforca@gmail.com>
2025-03-21 00:50:35 +0000

    desafio20\25

---
+++ .vscode/N1_su4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int mai(){
+
+    int pontosturisticos = 20;
+    int populacâo = 200;
+    char estado = 'B';
+    char codigo = "b01";
+    char nome [10] = "brasilia";
+
+
+
+
+
+}
