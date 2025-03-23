16aca2d2f23f7b709d6984a441010faf4b07d1df
Matheuspresidencia <mateusuniforca@gmail.com>
2025-03-23 15:40:18 +0000

    23\25

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,28 +1,20 @@
 #include <stdio.h>
 
 int main() {
-    char B01;
-    char b;
-    char Brasilia;
-    int populacao;
-    int PontosTuristicos;
-    float area;
-    float pib;
+    char codigo = 'B01';
+    char nome[10];
+    char brasilia,DestritoFederal;
+    int populacao = 40.00000;
+    int PontosTuristicos = 12;
+    float area = 26.000;
+    float pib = 84.2;
 
-    
-    
-
-
-
-
-
-
-      
-
-
-
-
-
+    printf("o codigo é: %c é: %s \n" ,codigo,nome);
+    printf("o nome é: %s\n");
+    printf("qual a pupulacao: %d\n");
+    printf("pontosTuristicos: %i\n");
+    printf("a area é: %f\n");
+    printf("o pib é: %e\n");
 
 
 }
