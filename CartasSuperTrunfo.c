a8d52c98663623f1b9001b75f8571b77f1558465
Matheuspresidencia <mateusuniforca@gmail.com>
2025-04-07 17:54:07 +0000

    memoria do dia 07/04/2025

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,21 +9,14 @@ int main() {
     float area = 26.000;
     float pib = 84.2;
 
-    //printf("digite seu codigo: \n" );
-    //pritf("digite seu nome: \n" );
-    //printf("digite a cidade: \n" );
-    //printf("digite os PontosTuristicos: \n");
-    //printf("digite a area: \n ");
-    //printf("digite o pib: \n" );
+    //printf("digite seu codigo\n" );
+    //pritf("digite seu nome\n" );
+    //printf("digite a cidade\n" );
+    //printf("digite os Pontos Turisticos\n");
+    //printf("digite a area\n ");
+    //printf("digite o pib\n" );
 
-
-
-    printf("o codigo é: %c\n");
-    printf("o nome é: %s");
-    printf("qual a pupulacao é: %f\n");
-    printf("pontosTuristicos é: %d\n");
-    printf("a area é: %f\n");
-    printf("o pib é: %e\n");
+    
 
     return 0;
 }
